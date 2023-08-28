b7c7742ef738835bef11043f6fa43f1d16dcf085
owner <owner@Oliviasmacbook.local>
2023-08-28 16:20:39 +0100

    A web3 gaming platfrom

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  optimizer: true,
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    testnet_aurora: {
+      url: "https://testnet.aurora.dev",
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+    },
+  },
+};
